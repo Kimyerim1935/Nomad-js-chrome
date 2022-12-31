e3eee153059eb7ed7d19d760c5980887d6f42f7b
kimyerim1935 <kimyerim1935@naver.com>
2022-12-31 17:02:48 +0900

    add: localStorage data set

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 const handleSubmit = (e) => {
   e.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const userName = loginInput.value;
-  greeting.innerText = "Hello " + userName;
+  localStorage.setItem('userName', userName);
+  paintGreetings(userName)
+}
+
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
 loginForm.addEventListener('submit', handleSubmit);
+
+const saveUserName = localStorage.getItem(USERNAME_KEY);
+
+if (saveUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleSubmit);
+} else {
+  paintGreetings();
+}
