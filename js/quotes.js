db0ccab95a19b1afeb0aa425f7a2ae5ff1c5f16d
kimyerim1935 <kimyerim1935@naver.com>
2023-01-02 23:29:32 +0900

    add: 명언 파일 생성

---
+++ js/quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+  {
+  quote: "Don't dwell on the past",
+  author: '-'
+},
+{
+  quote: "Seize the day",
+  author: '-'
+},
+{
+  quote: "You only live once",
+  author: '-'
+}
+]
