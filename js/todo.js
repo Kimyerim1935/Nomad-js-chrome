76e9f66082738d02efcb90a38b03f20ef0b9820c
kimyerim1935 <kimyerim1935@naver.com>
2023-01-06 23:02:08 +0900

    add: todo 컴포넌트 작성

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,20 @@
-const toDoForm = document.getElementById('todo-form');
-const todoInput = document.querySelector('#todo-form input');
-const todoList = document.getElementsById('todo-list');
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
 
-function handleToDoSubmit(e) {
-  e.preventDefault();
-  const newTodo = todoInput.value;
-  todoInput.value = '';
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
 }
 
-toDoForm.addEventListener('submit', handleToDoSubmit);
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
