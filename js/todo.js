bbbaf10be291b3b91bc1471242648f494e8dfeb9
kimyerim1935 <kimyerim1935@naver.com>
2023-01-07 23:13:34 +0900

    mod: li 삭제기능 추가

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo (event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "👹";
+  button.style.marginLeft = '5px';
+
+  button.addEventListener('click', deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
