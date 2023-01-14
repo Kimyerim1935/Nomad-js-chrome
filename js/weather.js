7732b5e43af0fb8d9ab2c38e7b93fdf2f0e28872
kimyerim1935 <kimyerim1935@naver.com>
2023-01-14 23:53:57 +0900

    add: api 호출

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,15 @@
-function onGeoOk() {
+function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
+    const API_KEY = "a37c799a1d407e868db800bb47a76cda";
 
     console.log("you live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+    console.log(url);
 }
 
-function onGeoError() {}
+function onGeoError() {
+    alert("Can't find you.");
+}
 
-navigator.geolocation.getCurrentPosition();
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
